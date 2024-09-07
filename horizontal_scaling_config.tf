41449b316dcb01a2651a1f770c62f9a089fa6589
Pat Patel <priyankkp@gmail.com.>
2024-09-07 07:27:00 +0000

    updated autoscaling features

--- horizontal_scaling_config.tf
+++ horizontal_scaling_config.tf
@@ -48,7 +48,7 @@ resource "aws_autoscaling_group" "autoscale_instance_parameters" {
   desired_capacity          = 1
   vpc_zone_identifier       = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id, aws_subnet.subnet_c.id]
   launch_template {
-    id      = aws_launch_template.aws_eks_ec2_instance_template
+    id      = aws_launch_template.aws_eks_ec2_instance_template.id
   }
 }
 
