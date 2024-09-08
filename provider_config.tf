625e365a64bd447c842307bcf777dc1195ae73b4
Pat Patel <priyankkp@gmail.com.>
2024-09-08 04:42:25 +0000

    updated provider_config file

--- provider_config.tf
+++ provider_config.tf
@@ -18,7 +18,7 @@ terraform {
 #The Access,secret keys are optional, and same with token, based on authentication requirements by company.
 provider "aws" {
   region      = "us-east-1"
-#  access_key  = var.access_key
-#  secret_key  = var.secret_key
-#  token       = var.token_key
+  access_key  = var.AWS_ACCESS_KEY_ID
+  secret_key  = var.AWS_SECRET_ACCESS_KEY
+  shared_credentials_file = ""
 }
