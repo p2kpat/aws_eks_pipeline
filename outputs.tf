47f4ea27232a9c42dbfbe5ae0d2d4e80c5eef9ee
Pat Patel <priyankkp@gmail.com.>
2024-09-11 10:38:42 +0000

    clear repo

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "master_role_arn" {
-  value = data.aws_iam_role.existing_master_role.arn
-}
-
-output "worker_role_arn" {
-  value = data.aws_iam_role.existing_worker_role.arn
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-ami_id           = "ami-0e86e20dae9224db8"
-AWS_REGION       = "us-east-1"
