0ab6e434955116fd75899c96f965cf8d76e4d61d
Pat Patel <priyankkp@gmail.com.>
2024-09-11 10:50:22 +0000

    fresh repo

--- locals_config.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  master_role_arn = aws_iam_role.aws_eks_master_node[0].arn
-  worker_role_arn = aws_iam_role.aws_eks_worker_node[0].arn
-}
-
-
--- terraform.tfvars
+++
@@ -1,1 +0,0 @@
-ami_id           = "ami-0e86e20dae9224db8"
